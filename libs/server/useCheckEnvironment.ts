5567bd9ae1a439b8b815a282e1c2c0ebef032b42
0hyeon <djdjdjk2006@naver.com>
2023-07-05 17:30:48 +0900

    fix: 배포주소 변경

--- libs/server/useCheckEnvironment.ts
+++ libs/server/useCheckEnvironment.ts
@@ -2,7 +2,7 @@ export const checkEnvironment = () => {
   let base_url =
     process.env.NODE_ENV === "development"
       ? "http://localhost:3000"
-      : "https://example.com";
+      : "https://web-crawaling-o8ml19eve-0hyeon-s-team.vercel.app";
 
   return base_url;
 };
