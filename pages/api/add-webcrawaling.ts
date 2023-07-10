ea6bb8b708af9dccacb9c460d3946a566ea41efe
0hyeon <djdjdjk2006@naver.com>
2023-07-10 15:13:05 +0900

    build: 배포환경시 ec2서버 사용

--- pages/api/add-webcrawaling.ts
+++ pages/api/add-webcrawaling.ts
@@ -13,11 +13,18 @@ interface MobileBanner {
 interface PCBanner extends MobileBanner {}
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
+  let url;
+  if (process.env.NODE_ENV === "development") {
+    url = "http://127.0.0.1/hello";
+  } else {
+    url = "http://43.202.29.183/hello";
+  }
+
   if (req.method === "GET") {
     try {
       // Step 1: Fetch the data
       const response = await (
-        await fetch("http://127.0.0.1/hello", {
+        await fetch(`${url}`, {
           method: "GET",
           headers: {
             "Content-Type": "application/json",
