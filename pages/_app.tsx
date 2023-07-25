5bb8f1f3a671213e19a6c083266898272d7f94c2
0hyeon <djdjdjk2006@naver.com>
2023-07-25 13:31:20 +0900

    fix _app.tsx 경로삭제

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import Layout from "@components/Layout";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   const queryClient = new QueryClient({
