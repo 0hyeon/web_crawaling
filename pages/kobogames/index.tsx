9630f5c2d52fa8de34fed8202730ecad5aef56dc
0hyeon <djdjdjk2006@naver.com>
2024-01-18 23:43:07 +0900

    feat: 크롤링 불러오기

--- pages/kobogames/index.tsx
+++ pages/kobogames/index.tsx
@@ -8,14 +8,17 @@ import {
   FileInputProps,
   Group,
   rem,
+  MultiSelect,
 } from "@mantine/core";
 import { IconPhoto } from "@tabler/icons-react";
 import { LoadingText, Svg } from "pages/exceltrans";
 import Loading from "public/asset/svg/Logo";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const KoboGames = () => {
   const [files, setFiles] = useState<File[]>([]);
+  const [isFetchData, setFetchData] = useState();
+  const [nameValues, setNameValues] = useState<string[]>([]);
   const [state, setSate] = useState({
     loading: false,
   });
@@ -43,6 +46,31 @@ const KoboGames = () => {
     setFiles(newFiles);
     setFormData(newFormData);
   }
+  const fetchData = async () => {
+    try {
+      setSate(() => ({ loading: true }));
+
+      const response = await fetch(
+        BEcheckEnvironment().concat("/api/kobogames/kobo_store"),
+        {
+          method: "GET",
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      setFetchData(data.data);
+      console.log(data);
+
+      setSate({ loading: false });
+    } catch (error) {
+      console.error(error);
+      setSate({ loading: false });
+    }
+  };
   function handleSubmit() {
     const formDataToSend = new FormData();
     files.forEach((file, index) => {
@@ -122,7 +150,7 @@ const KoboGames = () => {
 
   const labels = [""];
   const FileInputs = labels.map((el, index) => (
-    <Box key={index} maw={520} mx="auto">
+    <Box key={index} maw={1000} mx="auto">
       <FileInput
         mt="md"
         label={`${el}`}
@@ -133,56 +161,94 @@ const KoboGames = () => {
       />
     </Box>
   ));
+
+  console.log("isFetchData : ", isFetchData);
+  useEffect(() => {
+    if (isFetchData) {
+      const names = Object.keys(isFetchData);
+      setNameValues(names);
+    }
+  }, [isFetchData]);
+  console.log("nameValues : ", nameValues);
   return (
     <>
       <MenubarLeft />
       <div className="h-[100%] min-h-[100vh] w-full bg-[#dee2e6] pl-64">
         <div className="mx-4 min-h-[100vh] bg-white px-4 py-16">
           <div className="mx-auto w-[90%]">
-            {state?.loading === true ? (
-              <>
-                <Svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 448 512"
-                  width={100}
-                  height={100}
-                >
-                  <Loading />
-                </Svg>
-                <LoadingText>Loading...</LoadingText>
-              </>
-            ) : (
-              <div className="p-16">
-                <div className="mb-14 text-center font-sans text-2xl font-extrabold">
-                  [코보게임즈] 이커머스 Merge Logic Process [...ing]
-                </div>
-                <div className="mb-14 flex flex-col gap-1 text-center">
-                  <div className="mb-2">
-                    <b className="text-xl">재료:</b>
+            <div className="mx-auto w-[40%]">
+              {state?.loading === true ? (
+                <>
+                  <Svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 448 512"
+                    width={100}
+                    height={100}
+                  >
+                    <Loading />
+                  </Svg>
+                  <LoadingText>Loading...</LoadingText>
+                </>
+              ) : (
+                <div className="">
+                  <div className="mb-14 text-center font-sans text-2xl font-extrabold">
+                    [코보게임즈] 이커머스 Merge Logic Process [...ing]
                   </div>
-                  <div>
-                    <b>1.</b>(GB)KBG_이커머스_Daily_Report.xlsx
-                  </div>
-                </div>
-                {FileInputs}
-                <Box
-                  maw={520}
-                  mx="auto"
-                  mt="md"
-                  className="mt-8 flex items-center justify-center"
-                >
-                  <Button className="bg-black" onClick={handleSubmit}>
+                  {/* 호출하기 */}
+                  <Button className="bg-black" onClick={fetchData}>
                     전송
                   </Button>
-                  {/* <Button
-                className="bg-whie border-[2px] border-black text-black"
-                onClick={handleRemove}
-              >
-                비우기
-              </Button> */}
-                </Box>
-              </div>
-            )}
+
+                  {/* 상품설정 */}
+                  <div className="mx-auto mb-2 mt-20 w-[100%]">
+                    <MultiSelect
+                      label="상품명"
+                      placeholder="Pick value"
+                      data={nameValues && nameValues}
+                      searchable
+                    />
+                  </div>
+                  {/*  */}
+                  {/* <div>
+                    {isFetchData &&
+                      Object.entries(isFetchData).map(
+                        ([name, quantity]: [string, any]) => (
+                          <div key={name}>
+                            <p>{name}</p>
+                            <p>Quantity: {quantity}</p>
+                          </div>
+                        )
+                      )}
+                  </div> */}
+                  {/*  */}
+                  <div className="mb-14 flex flex-col gap-1 text-center">
+                    <div className="mb-2 mt-20">
+                      <b className="text-xl">재료:</b>
+                    </div>
+                    <div>
+                      <b>1.</b>(GB)KBG_이커머스_Daily_Report.xlsx
+                    </div>
+                  </div>
+                  {FileInputs}
+                  <Box
+                    maw={520}
+                    mx="auto"
+                    mt="md"
+                    className="mt-8 flex items-center justify-center"
+                  >
+                    <Button className="bg-black" onClick={handleSubmit}>
+                      전송
+                    </Button>
+                    {/* <Button
+                  className="bg-whie border-[2px] border-black text-black"
+                  onClick={handleRemove}
+                >
+                  비우기
+                </Button> */}
+                  </Box>
+                </div>
+              )}
+            </div>
           </div>
         </div>
       </div>
