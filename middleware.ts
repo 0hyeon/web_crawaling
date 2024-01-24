f0edd607db6d852e5d90075661023936045616c7
0hyeon <djdjdjk2006@naver.com>
2024-01-25 01:11:27 +0900

    feat: login 이슈처리

--- middleware.ts
+++ middleware.ts
@@ -1,27 +1,27 @@
-// import { FEcheckEnvironment } from "@libs/server/useCheckEnvironment";
-// import { NextResponse, userAgent } from "next/server";
-// import type { NextRequest, NextFetchEvent } from "next/server";
+import { FEcheckEnvironment } from "@libs/server/useCheckEnvironment";
+import { NextResponse, userAgent } from "next/server";
+import type { NextRequest, NextFetchEvent } from "next/server";
 
-// export function middleware(req: NextRequest, ev: NextFetchEvent) {
-//   //   const url = req.nextUrl.clone();
+export function middleware(req: NextRequest, ev: NextFetchEvent) {
+  //   const url = req.nextUrl.clone();
 
-//   //    // Check if the request is trying to access secured routes without the session
-//   //    const isRestrictedPath =
-//   //    req.nextUrl.pathname.startsWith("/pcbanner") ||
-//   //    req.nextUrl.pathname.startsWith("/mbanner") ||
-//   //    req.nextUrl.pathname.startsWith("/exceltrans") ||
-//   //    req.nextUrl.pathname.startsWith("/exceltransAlbamon") ||
-//   //    req.nextUrl.pathname.startsWith("/excelLogic");
+  //    // Check if the request is trying to access secured routes without the session
+  //    const isRestrictedPath =
+  //    req.nextUrl.pathname.startsWith("/pcbanner") ||
+  //    req.nextUrl.pathname.startsWith("/mbanner") ||
+  //    req.nextUrl.pathname.startsWith("/exceltrans") ||
+  //    req.nextUrl.pathname.startsWith("/exceltransAlbamon") ||
+  //    req.nextUrl.pathname.startsWith("/excelLogic");
 
-//   //  const isLoggedIn = req.cookies.get("gb_session") !== undefined;
+  //  const isLoggedIn = req.cookies.get("gb_session") !== undefined;
 
-//   //  if (isRestrictedPath && !isLoggedIn) {
-//   //    // Check if the request is not already being redirected to the login page
-//   //    if (!req.nextUrl.pathname.startsWith("/login")) {
-//   //      return NextResponse.redirect(new URL("/login", req.url));
-//   //    }
-//   //  }
+  //  if (isRestrictedPath && !isLoggedIn) {
+  //    // Check if the request is not already being redirected to the login page
+  //    if (!req.nextUrl.pathname.startsWith("/login")) {
+  //      return NextResponse.redirect(new URL("/login", req.url));
+  //    }
+  //  }
 
-//   // Allow requests to pass through for other cases
-//   return NextResponse.next();
-// }
+  // Allow requests to pass through for other cases
+  return NextResponse.next();
+}
